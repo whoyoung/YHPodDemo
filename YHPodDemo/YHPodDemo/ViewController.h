d4dfd478a48916d30be2c35c93133c0908fbb8db
yanghu <huyang@mail.bistu.edu.cn>
2018-08-03 23:24:38 +0800

    initial demo

---
+++ YHPodDemo/YHPodDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YHPodDemo
+//
+//  Created by young on 2018/8/3.
+//  Copyright © 2018年 young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YHPodDemo/YHPodDemo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  YHPodDemo
+//
+//  Created by young on 2018/8/3.
+//  Copyright © 2018年 young. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <WMAdSDK/WMAdSDK.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
